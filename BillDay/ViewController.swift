51456eea23856e839d84fe943e1974636cda0866
jaimetejeiro <jaimetejeiro@gmail.com>
2020-06-04 20:42:44 +0200

    Initial Commit

---
+++ BillDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BillDay
+//
+//  Created by Jaime Tejeiro on 04/06/2020.
+//  Copyright © 2020 Jtejeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
